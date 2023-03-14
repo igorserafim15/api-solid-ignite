894ff3fe4c31e39adad3c543d8ee752a95afa3fc
Igor Serafim <igor.serafim@nolifretes.com.br>
2023-03-13 22:03:05 -0300

    first unit tests

--- src/services/register.ts
+++ src/services/register.ts
@@ -1,11 +1,16 @@
 import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 
-interface RegisterServiceRequest {
+interface RegisterServiceReq {
   name: string
   email: string
   password: string
+}
+
+interface RegisterServiceRes {
+  user: User
 }
 
 export class RegisterService {
@@ -13,7 +18,7 @@ export class RegisterService {
     //
   }
 
-  async execute(body: RegisterServiceRequest) {
+  async execute(body: RegisterServiceReq): Promise<RegisterServiceRes> {
     const userEmailAlreadyExists = await this.usersRepository.findByEmail(
       body.email,
     )
@@ -24,10 +29,12 @@ export class RegisterService {
 
     const password_hash = await hash(body.password, 6)
 
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name: body.name,
       email: body.email,
       password_hash,
     })
+
+    return { user }
   }
 }
