59ef08712819b5a7b61d3f34b617724be32d4cf5
Igor Serafim <igorserafim.contato@gmail.com>
2023-03-19 13:35:33 -0300

    teste

--- src/env/index.ts
+++ src/env/index.ts
@@ -10,7 +10,6 @@ const _env = envSchema.safeParse(process.env)
 
 if (_env.success === false) {
   console.error('❌ Invalid environment variables', _env.error.format())
-
   throw new Error('Invalid environment variables.')
 }
 
